48aa6a73663387483d04b891f42320cc6c00305a
leoguimaraes49 <leoguimasvm@gmail.com>
2025-02-03 00:10:58 -0300

    pequenas correções

--- setup.py
+++ setup.py
@@ -4,15 +4,5 @@ setup(
     name="projeto_cafe",
     version="0.1",
     packages=find_packages(),  # Encontra automaticamente "agents", "api", etc.
-    install_requires=[
-        "fastapi",
-        "uvicorn",
-        "sounddevice",
-        "whisper-openai",
-        "numpy",
-        "torch",
-        "torchaudio",
-        "soundfile",
-        "git+https://github.com/facebookresearch/audiocraft.git"
-    ],
+    
 )
